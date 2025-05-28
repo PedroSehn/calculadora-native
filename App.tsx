23827378bd896c7d77bc44319f3fa7b6b127b1e0
PedroSehn <pedrorsehn@hotmail.com>
2025-05-28 20:19:06 -0300

    adiciona app completo de calculadora

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Switch} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { ThemeContext} from "./src/contex/ThemeContext";
+import { useState} from "react";
+import {Button} from "./src/components/Button";
+import {myColors} from "./src/styles/Colors";
+import {MyKeyboard} from "./src/components/MyKeyboard";
+import {Styles} from "./src/styles/GlobalStyles"
 
 export default function App() {
+  const [theme, setTheme] = useState('light');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaProvider>
+    <ThemeContext.Provider value={theme}>
+    <SafeAreaView style={theme === "light" ? styles.container : [styles.container, {backgroundColor: myColors.dark}]}>
+      <Switch
+        value={theme === "light"}
+        onValueChange={() => setTheme(theme === "light" ? "dark" : "light")}/>
+      <Text style={theme === "light" ? Styles.smallTextDark : Styles.smallTextLight }>{`${theme} mode`}</Text>
+      <MyKeyboard/>
+    </SafeAreaView>
+    </ThemeContext.Provider>
+      </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
